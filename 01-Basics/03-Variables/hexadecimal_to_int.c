9008e9e266a563b17b428158ec4b79257b2096ef
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-09-14 10:14:20 +0500

    Hex to Integer Basic program Added in variables

--- 01-Basics/03-Variables/hexadecimal_to_int.c
+++ 01-Basics/03-Variables/hexadecimal_to_int.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 
-int hexToInteger(char *string) {}
+int hexToInteger(char *string) {
+  int i = 0;
+  int result = 0;
 
-int main() { return 0; }
+  if (string[0] == '0' && (string[1] == 'x' || string[1] == 'X')) {
+    i = 2;
+  }
+
+  while (string[i] != '\0') {
+    int val = 0;
+
+    if (string[i] >= '0' && string[i] <= '9') {
+      val = string[i] - '0';
+    } else if (string[i] >= 'a' && string[i] <= 'f') {
+      val = string[i] - 'a' + 10;
+    } else if (string[i] >= 'A' && string[i] <= 'F') {
+      val = string[i] - 'A' + 10;
+    } else {
+      break;
+    }
+
+    result = (result * 16) + val;
+    i++;
+  }
+
+  return result;
+}
+
+int main() {
+  char hexNoPrefix[] = "1A3F";
+  char hexWithPrefix[] = "0x1A3F";
+
+  int value1 = hexToInteger(hexNoPrefix);
+  int value2 = hexToInteger(hexWithPrefix);
+
+  printf("No Prefix: %d\n", value1);
+  printf("With Prefix: %d\n", value2);
+
+  return 0;
+}
